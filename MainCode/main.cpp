cb907c75d0b2c6e5e4650f14ceeb8f0d86e8b48a
Mads <madsn2000@gmail.com>
2024-03-15 17:08:51 +0100

    V1 - Terminal Besked

---
+++ MainCode/main.cpp
@@ -0,0 +1,9 @@
+#include <QCoreApplication>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "V1 Running - Time to begin the semester project :)" << std::endl;
+
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+2. Semester Gripper Projekt
+
+Af:
+
+- Emil
+- Mads
+- Rune
+- Thomas
+- Nicki
+- Oliver
+- Poul
+
+
