fd69d41c3a30521b27b25c0bbbf788decd18054d
RaspberryPi <RaspberryPi@sdu.com>
2024-04-23 13:04:40 +0200

    Slettet Filer mm.

--- build-URTCP_Test1-Desktop-Debug/CMakeFiles/URTCP_Test1.dir/URTCP_Test1_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/URTCP_Test1.dir/URTCP_Test1_autogen/mocs_compilation.cpp.o: \
- /mnt/c/Users/Poul/Desktop/SYNC/SDU/2.\ semester/semesterprojekt/URTCP_devSpace/GripperGit/build-URTCP_Test1-Desktop-Debug/URTCP_Test1_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
--- build-URTCP_Test1-Desktop-Debug/CMakeFiles/URTCP_Test1.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for URTCP_Test1.
-# This may be replaced when dependencies are built.
